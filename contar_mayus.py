be543c3dcca0f59f54db0d08f15c5f3962c67b3d
NioDevp <139724171+NioDevp@users.noreply.github.com>
2023-10-20 17:08:37 +0200

    Update contar_mayus.py

--- contar_mayus.py
+++ contar_mayus.py
@@ -1,9 +1,9 @@
 import string
 
-texto_user = input("Introduzca un texto: ")
+text_user = input("Introduzca un texto: ")
 mayusculas = 0
 
-for letras in texto_user:
+for letras in text_user:
     if letras in string.ascii_uppercase:
         mayusculas +=1
 
