81a91db17f2c79b0ea4819eb82442aa17c3bc168
NioDevp <139724171+NioDevp@users.noreply.github.com>
2023-10-20 17:08:03 +0200

    Update adivinar_numero.py

--- adivinar_numero.py
+++ adivinar_numero.py
@@ -1,3 +1,12 @@
+"""En este código, te propongo unas variaciones que tendrás que crear tu para practicar
+PRACTICA 1: Haz que el programa se repita en bucle hasta que se adivine el número secreto
+PRÁCTICA 2: Haz que el número secreto cambie en cada partida
+
+In this code, I suggest some variations that you will need to create yourself to practice.
+PRACTICE 1: Make the program repeat in a loop until the secret number is guessed.
+PRACTICE 2: Make the secret number change in each game.
+"""
+
 print("Intenta adivnar el número del 1 al 10 en el que estoy pensando")
 
 intento_user = int(input("En que número crees que estoy pensando: "))
