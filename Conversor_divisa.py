e27bb1c8ade6234c2c3f6f3dba28e283e174d542
NioDevp <139724171+NioDevp@users.noreply.github.com>
2023-10-20 16:58:10 +0200

    Update Conversor_divisa.py

--- Conversor_divisa.py
+++ Conversor_divisa.py
@@ -6,22 +6,22 @@ euro_dolar = 1,11
 libra_euro = 1.17
 euro_libra = 0.86
 
-modo = input("¿Que conversión quiere hacer?\n"
+mode = input("¿Que conversión quiere hacer?\n"
              "A - Dolar a Euro\n"
              "B - Euro a Dolar\n"
              "C - Libra a Euro\n"
              "D - Euro a Libra\n")
 
-if modo == "A":
+if mode == "A":
     dolar = float(input("Introduzca la cantidad de dolares que quiere convertir a euros: "))
     print("{} dolares son {} euros".format(dolar, dolar * dolar_euro))
-elif modo == "B":
+elif mode == "B":
     euro = float(input("Introduzca la cantidad de euros que quiere convertir a dolares: "))
     print("{} euros son {} dolares".format(euro, euro * euro_dolar))
-elif modo == "C":
+elif mode == "C":
     libra = float(input("Introduzca la cantidad de libras que quiere convertir a euros: "))
     print("{} libras son {} euros".format(libra, libra * libra_euro))
-elif modo == "D":
+elif mode == "D":
     euro = float(input("Introduzca la cantidad de euros que quiere convertir a libras:"))
     print("{} euros son {} libras".format(euro, euro * euro_libra))
 else:
