80d183379f441866eda6cb63ab6b85f8e2a9a7b8
NioDevp <139724171+NioDevp@users.noreply.github.com>
2023-10-20 17:11:18 +0200

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,3 +1,11 @@
+"""En este código se hace uso de funciones para imprimir por pantalla la secuencia de 
+Fibonacci. Como práctica, te animo a mejorar la interacción con el usuario final añadiendo, por
+ejemplo, un mensaje al principio del programa.
+
+In this code, functions are used to print the Fibonacci sequence to the screen. As an exercise,
+I encourage you to enhance the interaction with the end user by, for instance, adding a message 
+at the beginning of the program."""
+
 def fibonacci(n):
     if n <= 1:
         return 1
