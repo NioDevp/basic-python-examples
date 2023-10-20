543d37bc520f07c1b3ccf2de043c0194e2c16e0e
NioDevp <139724171+NioDevp@users.noreply.github.com>
2023-10-20 17:09:12 +0200

    Update fahrenhaet_celsius.py

--- fahrenhaet_celsius.py
+++ fahrenhaet_celsius.py
@@ -1,6 +1,8 @@
-#Fahrenheit a Celsius
+#Fahrenheit to Celsius
 
 print(("Conversor de Grados Fahreheit (ºF) a Grados Celsius (ºC)"))
+
 fahrenheit = float(input("Introduce los grados Fahrenheit: "))
 celsius = (fahrenheit-32)*5/9
+
 print("{} grados Fahrenheit son {} grados celsius".format(fahrenheit, celsius))
