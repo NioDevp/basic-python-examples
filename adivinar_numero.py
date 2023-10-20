34b27e8c91ac6500227f7877c3b23decf183c6d4
NioDevp <139724171+NioDevp@users.noreply.github.com>
2023-10-20 16:58:52 +0200

    Update adivinar_numero.py

--- adivinar_numero.py
+++ adivinar_numero.py
@@ -1,9 +1,9 @@
 print("Intenta adivnar el número del 1 al 10 en el que estoy pensando")
 
 intento_user = int(input("En que número crees que estoy pensando: "))
-num_secreto = 7
+num_sec = 7
 
-if intento_user == num_secreto:
-    print("Felicidades, el numero en el que estaba pensando era {}".format(num_secreto))
+if intento_user == num_sec:
+    print("Felicidades, el numero en el que estaba pensando era {}".format(num_sec))
 
 print("Juego terminado")
